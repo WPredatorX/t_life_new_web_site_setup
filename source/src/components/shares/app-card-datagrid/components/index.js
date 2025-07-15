506398a37c3d836387f92afcb15b598145a44954
WPredatorX <pee05555@gmail.com>
2025-07-15 11:52:11 +0700

    remove all

--- source/src/components/shares/app-card-datagrid/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as AppProductSaleRange } from "./appProductSaleRange";
-export { default as AppProductSalePaidType } from "./appProductSalePaidType";
-export { default as AppProductSalePaidCategory } from "./appProductSalePaidCategory";
-export { default as AppProductSalePrepayment } from "./appProductSalePrepayment";
-export { default as AppProductSaleTemplate } from "./appProductSaletemplate";
